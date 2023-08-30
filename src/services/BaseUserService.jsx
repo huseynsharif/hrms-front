bdce8616c5edc515498bd10f124b6a5f499d7cd6
Huseyn Sharifzade <huseynsharif@gmail.com>
2023-08-30 19:48:29 +0400

    Login connected to backend

--- src/services/BaseUserService.jsx
+++ src/services/BaseUserService.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export class BaseUserService{
 
     findByEmail(email, password){
-        return axios.get("http://localhost:8080/api/users/findbyemail", email, password)
+        return axios.get(`http://localhost:8080/api/users/findbyemail?email=${email}&password=${password}`)
     }
 
 }
